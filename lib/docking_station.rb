5a46625baae3d005d7eea68be0bee947a6a4da7f
Jamie Allen <Jamieallen59@gmail.com>
2014-09-16 15:45:24 +0100

    Added Docking Station functionality

---
+++ lib/docking_station.rb
@@ -0,0 +1,26 @@
+class DockingStation
+
+	DEFAULT_CAPACITY = 20
+
+	def initialize( options = {} )
+		@capacity = options.fetch(:capacity, DEFAULT_CAPACITY)
+		@bikes = []
+	end
+
+	def dock(bike)
+		@bikes << bike
+	end
+
+	def bike_count
+		@bikes.count
+	end
+
+	def release(bike)
+		@bikes.delete(bike)
+	end
+
+	def full?
+		bike_count == @capacity
+	end
+
+end
---
+++ spec/docking_station_spec.rb
@@ -0,0 +1,26 @@
+require 'docking_station.rb'
+
+describe DockingStation do
+
+	let (:bike) { Bike.new }
+	let (:station) { DockingStation.new(:capacity => 20) }
+
+	it "should hold a bike" do
+		expect(station.bike_count).to eq 0
+		station.dock(bike)
+		expect(station.bike_count).to eq 1
+	end
+
+	it "should release a bike" do
+		station.dock(bike)
+		expect(station.bike_count).to eq 1
+		station.release(bike)
+		expect(station.bike_count).to eq 0
+	end
+
+	it "should know when it's full" do
+		expect(station).not_to be_full
+		20.times { station.dock(bike) }
+		expect(station).to be_full
+	end
+end
